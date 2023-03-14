42e24e48e306787eca5ad564393c73b27ea64039
Michael Kreil <github@michael-kreil.de>
2023-03-14 17:08:57 +0100

    add streamFileData

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 "use strict"
 
+const { createReadStream } = require('fs');
+const http = require('http');
+const https = require('https');
+const papaparse = require('papaparse');
 const os = require('os');
+const { extname } = require('path');
+const { Transform } = require('stream');
+const { StringDecoder } = require('string_decoder');
+const { createGunzip, createBrotliDecompress } = require('zlib');
+
+module.exports = {
+	streamFileData,
+};
 
 Array.prototype.forEachAsync = forEachAsync;
 
@@ -47,4 +59,79 @@ function forEachAsync() {
 			if (running < maxParallel) queueMicrotask(next);
 		}
 	})
+}
+
+async function streamFileData(filename) {
+	let stream;
+	if (filename.startsWith('http://')) {
+		stream = await getHttpStream(http, filename)
+	} else if (filename.startsWith('https://')) {
+		stream = await getHttpStream(https, filename)
+	} else {
+		stream = createReadStream(filename);
+	}
+
+	if (filename.endsWith('.gz')) {
+		filename = filename.slice(0, -3);
+		stream = stream.pipe(createGunzip())
+	} else if (filename.endsWith('.br')) {
+		filename = filename.slice(0, -3);
+		stream = stream.pipe(createBrotliDecompress())
+	}
+
+	if (/\.[ct]sv$/.test(filename)) {
+		return getCsvParser(stream);
+	} else if (/\.(geo)?json(l|seq)$/.test(filename)) {
+		return getJsonParser(stream);
+	} else {
+		throw new Error('Unknown file extension: ' + extname(filename));
+	}
+
+	function getHttpStream(lib, url) {
+		return new Promise(res => {
+			lib.request(url, stream => res(stream)).end();
+		})
+	}
+
+	function getSplitter(matcher = /\r?\n/, format = 'utf8') {
+		let last = '';
+		let decoder = new StringDecoder(format);
+
+		return new Transform({
+			autoDestroy: true,
+			readableObjectMode: true,
+			transform,
+			flush,
+		})
+
+		function transform(chunk, enc, cb) {
+			last += decoder.write(chunk);
+			let lines = last.split(matcher);
+			let lastIndex = lines.length - 1;
+			for (let i = 0; i < lastIndex; i++) this.push(lines[i])
+			last = lines[lastIndex];
+			cb();
+		}
+
+		function flush(cb) {
+			this.push(last);
+			cb();
+		}
+	}
+
+	function getCsvParser(stream) {
+		let parser = papaparse.parse(papaparse.NODE_STREAM_INPUT, { header: true });
+		return stream.pipe(parser);
+	}
+
+	function getJsonParser(stream) {
+		stream = stream.pipe(getSplitter());
+		return stream.pipe(new Transform({
+			autoDestroy: true,
+			readableObjectMode: true,
+			transform: (line, enc, cb) => {
+				cb(null, (line.length > 0) ? JSON.parse(line) : null)
+			},
+		}))
+	}
 }
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 "use strict"
 
-test()
+const { streamFileData } = require('./index.js');
 
-async function test() {
-	require('./index.js');
+(async function () {
+	await testForEach();
+	await testStreamFileData();
+})()
 
-	let list = [1,1,2,3,5];
+async function testForEach() {
+	let list = [1, 1, 2, 3, 5];
 
 	list.forEach(item => {
 		item += 2;
@@ -21,4 +24,14 @@ async function test() {
 		await new Promise(res => setTimeout(res, 1));
 	}, 3)
 
+}
+
+
+async function testStreamFileData() {
+	let stream = await streamFileData('https://storage.googleapis.com/datenhub-net-static/data/test.geojsonl.br');
+	//let stream = await streamFileData('test.tsv.br');
+	//let stream = await streamFileData('https://storage.googleapis.com/datenhub-net-static/data/test.tsv.br');
+	for await (let line of stream) {
+		console.log(line);
+	}
 }
