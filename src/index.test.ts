58f1f57d26ce0f84ccc433650d5c4379f8fe3c88
Michael Kreil <github@michael-kreil.de>
2024-10-01 20:30:04 +0200

    add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,35 +1,44 @@
-"use strict"
-
 import { streamFileData, forEachAsync } from './index.js';
 
-(async function () {
-	await testForEach();
-	await testStreamFileData();
-})()
+describe('Utility Functions', () => {
 
-async function testForEach() {
-	const list = [1, 1, 2, 3, 5];
+	describe('forEachAsync', () => {
+		it('should process a list of numbers asynchronously', async () => {
+			const list = [1, 1, 2, 3, 5];
 
-	list.forEach(item => {
-		item += 2;
-	})
+			// First synchronous forEach
+			list.forEach((item, index) => {
+				list[index] = item + 2;
+			});
 
-	await forEachAsync(list, async item => {
-		item += 2;
-		await new Promise(res => setTimeout(res, 1));
-	})
+			// First forEachAsync with default concurrency
+			await forEachAsync(list, async (item, index) => {
+				list[index] = item + 2;
+				await new Promise(res => setTimeout(res, 1));
+			});
 
-	await forEachAsync(list, async item => {
-		item += 2;
-		await new Promise(res => setTimeout(res, 1));
-	}, 3)
+			// Second forEachAsync with concurrency of 3
+			await forEachAsync(list, async (item, index) => {
+				list[index] = item + 2;
+				await new Promise(res => setTimeout(res, 1));
+			}, 3);
 
-}
+			// Verify the expected list values
+			expect(list).toEqual([7, 7, 8, 9, 11]);
+		});
+	});
 
+	describe('streamFileData', () => {
+		it('should stream file data without errors', async () => {
+			const file = 'test/test.csv.br';
+			const stream = await streamFileData(file, { progress: true, fast: true });
 
-async function testStreamFileData() {
-	const stream = await streamFileData('https://storage.googleapis.com/datenhub-net-static/data/p075_stadt_land_post/zensus_points.csv.br', { progress: true, fast: true });
-	for await (const line of stream) {
-		
-	}
-}
+			for await (const _line of stream) {
+				// Here we would normally verify the contents of the line, but for now, we just iterate.
+			}
+
+			// Add an assertion to ensure the stream operation completed
+			expect(stream).toBeDefined();
+		});
+	});
+});
